cd69d3286ed4be368e3c01011839f9bf7975b35a
Calebe Machado <calebe.machado@platformbuilders.io>
2021-12-16 12:50:12 -0300

    add example on single file

---
+++ login.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+
+module.exports = () => {
+  const signUpRouter = new SignUpRouter()
+  router.post('/signup', ExpressRouterAdapter.adapt(signUpRouter))
+}
+
+class ExpressRouterAdapter {
+  static adapt (router) {
+    return async (req, res) => {
+      const httpRequest = {
+        body: req.body
+      }
+      const httpResponse = await router.route(httpRequest)
+      res.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+  }
+}
+
+// Presentation - Router
+class SignUpRouter {
+  async route (httpRequest) {
+    const { email, password, repeatPassword } = httpRequest.body
+    const user = new SignUpUseCase().signUp(email, password, repeatPassword)
+
+    return {
+      statusCode: 200,
+      body: user
+    }
+  }
+}
+
+// Domain - Use-case
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// Infra-layer - Repository
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
+  }
+}
